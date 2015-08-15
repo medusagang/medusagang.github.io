1daf32e54dd40e268c0ce358dd44412fa263a5ea
abrn <abrin@l.cn>
2015-08-15 02:09:28 +0000

    main commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main</title>
+    <link type="text/css" rel="stylesheet" href="./css/materialize.min.css" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<nav class="black" role="navigation">
+    <div class="nav-wrapper container">
+    </div>
+</nav>
+
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <br><br>
+            <h1 class="header center white-text text-lighten-2"></h1>
+        </div>
+    </div>
+    <div class="parallax">
+        <img src="img/medusa.png">
+    </div>
+</div>
+
+
+<div class="container black">
+    <div class="section black">
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="icon-block">
+                    <h2 class="center white-text"><i class="material-icons">Welcome to the official site of Team Medusa</i></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="icon-block">
+                    <h3 class="center white-text">Greets from <span style="color: #2EFE2E; ">Apollo, Athena, Louie, Variety, Convict</span></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<iframe width="0" height="0" src="http://www.youtube.com/embed/F4wXX_ucp6s?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+
+<script type="text/javascript" src="./js/jquery.min.js"></script>
+<script type="text/javascript" src="./js/materialize.min.js"></script>
+</body>
+</html>
